67bb6e5b3fa0f600e694db92ef8cbfcf544d83f8
Bruno Jay <brunojay001@gmail.com>
2021-12-13 14:25:16 +0300

    fixed tasklist

--- client/src/app/mis-components/tasklist/tasklist.component.ts
+++ client/src/app/mis-components/tasklist/tasklist.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {Router} from "@angular/router";
+import {TasklistService} from "../../services/tasklist.service";
 
 @Component({
   selector: 'app-tasklist',
@@ -6,8 +8,36 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./tasklist.component.css']
 })
 export class TasklistComponent implements OnInit {
+  entries: number = 10;
+  selected: any[] = [];
+  temp = [];
+  activeRow: any;
+  rows: Object[];
 
-  constructor() { }
+  constructor( private router: Router, private tasklistService: TasklistService) {
+  }
+
+  entriesChange($event) {
+    this.entries = $event.target.value;
+  }
+  filterTable($event) {
+    let val = $event.target.value;
+    this.rows = this.rows.filter(function(d) {
+      for (var key in d) {
+        if (d[key].toLowerCase().indexOf(val) !== -1) {
+          return true;
+        }
+      }
+      return false;
+    });
+  }
+  onSelect({ selected }) {
+    this.selected.splice(0, this.selected.length);
+    this.selected.push(...selected);
+  }
+  onActivate(event) {
+    this.activeRow = event.row;
+  }
 
   ngOnInit(): void {
   }
