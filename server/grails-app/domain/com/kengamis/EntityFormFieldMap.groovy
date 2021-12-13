9ac785b45e3c02bf8ccd5b304ae6fe9b134cc287
BryanNahabwe <nahabwebryn@hotmail.com>
2021-12-13 14:51:41 +0300

    Add few things about the entity

---
+++ server/grails-app/domain/com/kengamis/EntityFormFieldMap.groovy
@@ -0,0 +1,15 @@
+package com.kengamis
+
+class EntityFormFieldMap {
+
+    String id
+    String formField
+    String entityField
+    Date	dateCreated
+    Date	lastUpdated
+
+    static	belongsTo	= [entityForm: EntityForm]
+
+    static constraints = {
+    }
+}
