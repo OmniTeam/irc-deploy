be32f307282d6a42a733fd9b672e45939381c74d
Bruno Jay <brunojay001@gmail.com>
2022-01-21 16:47:05 +0300

    added dummy data

--- client/src/app/mis-components/form-view/form-view.module.ts
+++ client/src/app/mis-components/form-view/form-view.module.ts
@@ -7,16 +7,18 @@ import {FormsModule} from "@angular/forms";
 import {NgxDatatableModule} from "@swimlane/ngx-datatable";
 import {ComponentsModule} from "../../components/components.module";
 import {DataTablesModule} from "angular-datatables";
+import {EntityViewTableModule} from "../entity-views/entity-view-table/entity-view-table.module";
 
 @NgModule({
   declarations: [FormViewComponent],
-  imports: [
-    CommonModule,
-    FormViewRoutingModule,
-    FormsModule,
-    NgxDatatableModule,
-    ComponentsModule,
-    DataTablesModule,
-  ]
+    imports: [
+        CommonModule,
+        FormViewRoutingModule,
+        FormsModule,
+        NgxDatatableModule,
+        ComponentsModule,
+        DataTablesModule,
+        EntityViewTableModule,
+    ]
 })
 export class FormViewModule { }
