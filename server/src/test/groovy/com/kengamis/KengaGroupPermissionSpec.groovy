1cbdcbdd5af9c0530ff46dc8cd61b698c37ec157
vkakama@gmail.com <vkakama@gmail.com>
2022-02-22 18:30:59 +0300

    remove dummy classes

---
+++ server/src/test/groovy/com/kengamis/KengaGroupPermissionSpec.groovy
@@ -0,0 +1,18 @@
+package com.kengamis
+
+import grails.testing.gorm.DomainUnitTest
+import spock.lang.Specification
+
+class KengaGroupPermissionSpec extends Specification implements DomainUnitTest<KengaGroupPermission> {
+
+    def setup() {
+    }
+
+    def cleanup() {
+    }
+
+    void "test something"() {
+        expect:"fix me"
+            true == false
+    }
+}
