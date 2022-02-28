49bd51ff378c7a140855d8cab0eae3730ec8b401
vkakama@gmail.com <vkakama@gmail.com>
2022-02-28 10:03:53 +0300

    added kenga group table for mapping kenga groups to roles

---
+++ server/src/test/groovy/com/kengamis/KengaGroupRoleSpec.groovy
@@ -0,0 +1,18 @@
+package com.kengamis
+
+import grails.testing.gorm.DomainUnitTest
+import spock.lang.Specification
+
+class KengaGroupRoleSpec extends Specification implements DomainUnitTest<KengaGroupRole> {
+
+    def setup() {
+    }
+
+    def cleanup() {
+    }
+
+    void "test something"() {
+        expect:"fix me"
+            true == false
+    }
+}
