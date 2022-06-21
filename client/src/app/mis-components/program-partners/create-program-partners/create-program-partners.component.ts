f195e06a46a9eb9a132099b0d8008f70f4aa8f2f
Okello Marvin <okellomarvinkevin@gmail.com>
2022-06-21 17:00:23 +0300

    Group is created when creating a partner

--- client/src/app/mis-components/program-partners/create-program-partners/create-program-partners.component.ts
+++ client/src/app/mis-components/program-partners/create-program-partners/create-program-partners.component.ts
@@ -1,13 +1,14 @@
 import {Component, OnInit} from '@angular/core';
-import {FormBuilder, FormGroup, Validators} from "@angular/forms";
-import {ActivatedRoute, Router} from "@angular/router";
-import {AlertService} from "../../../services/alert";
-import {ProgramPartnersService} from "../../../services/program-partners.service";
-import {CountriesService} from "../../../services/countries.service";
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
+import {ActivatedRoute, Router} from '@angular/router';
+import {AlertService} from '../../../services/alert';
+import {ProgramPartnersService} from '../../../services/program-partners.service';
+import {CountriesService} from '../../../services/countries.service';
 import {v4 as uuid} from 'uuid';
-import {CellEdit, OnUpdateCell} from "../../../helpers/cell-edit";
-import {UsersService} from "../../../services/users.service";
-import {Validator} from "../../../helpers/validator";
+import {CellEdit, OnUpdateCell} from '../../../helpers/cell-edit';
+import {UsersService} from '../../../services/users.service';
+import {Validator} from '../../../helpers/validator';
+import {GroupsService} from '../../../services/groups.service';
 
 @Component({
   selector: 'app-create-program-partners',
@@ -25,6 +26,9 @@ export class CreateProgramPartnersComponent implements OnInit, OnUpdateCell {
   cities = [];
   organisationsInvolved: any = [];
   username: string;
+  groupName: any;
+  groupParent: any;
+  groupCreationFormGroup: FormGroup;
 
   constructor(private formBuilder: FormBuilder,
               private route: ActivatedRoute,
@@ -32,6 +36,7 @@ export class CreateProgramPartnersComponent implements OnInit, OnUpdateCell {
               private countriesService: CountriesService,
               private router: Router,
               private usersService: UsersService,
+              private groupsService: GroupsService,
               private programPartnersService: ProgramPartnersService) {
   }
 
@@ -76,11 +81,31 @@ export class CreateProgramPartnersComponent implements OnInit, OnUpdateCell {
 
     this.formGroup.patchValue({organisationsInvolved: JSON.stringify(this.organisationsInvolved)});
     const programPartner = this.formGroup.value;
-    console.log("Form Data", programPartner)
+    console.log('Form Data', programPartner);
     this.programPartnersService.createProgramPartner(programPartner).subscribe(results => {
       this.alertService.success(`${programPartner.cluster} has been successfully created `);
     }, error => {
       this.alertService.error(`${programPartner.cluster} could not be created`);
+    });
+
+    // collecting variables for creating the group
+    this.groupName = programPartner['cluster'];
+    this.groupParent = programPartner['program'];
+
+    // creating a form group for creating a group
+    this.groupCreationFormGroup = this.formBuilder.group({
+      name: [this.groupName],
+      parentGroupId: [this.groupParent],
+      users: [null]
+    });
+
+    // creating a group after creating a partner
+    console.log(this.groupCreationFormGroup.value, 'group controls');
+    this.groupsService.createGroup(JSON.stringify(this.groupCreationFormGroup.value)).subscribe((result) => {
+      console.warn(result, 'Group created Successfully');
+      this.alertService.success(`Group has been created`);
+    }, error => {
+      this.alertService.error('Failed to Create the Group');
     });
 
     if (this.formGroup.valid) {
@@ -95,7 +120,7 @@ export class CreateProgramPartnersComponent implements OnInit, OnUpdateCell {
   onSelectCountry(country) {
     this.countriesService.getCitiesForCountry(country).subscribe((response) => {
       this.cities = response.data;
-    }, error => console.log(error))
+    }, error => console.log(error));
   }
 
   onReset() {
@@ -105,27 +130,27 @@ export class CreateProgramPartnersComponent implements OnInit, OnUpdateCell {
 
   addOrganization() {
     if (this.organisationsInvolved.length < 5) {
-      let id = uuid();
+      const id = uuid();
       this.organisationsInvolved.push({id: id, name: '', contact: ''});
     }
   }
 
   validateNumber(value) {
-    this.inValidNumber = Validator.telephoneNumber(value)
+    this.inValidNumber = Validator.telephoneNumber(value);
   }
 
-  cellEditor(rowId, tdId, key: string, oldValue, type?:string) {
+  cellEditor(rowId, tdId, key: string, oldValue, type?: string) {
     new CellEdit().edit(rowId, tdId, oldValue, key, this.saveCellValue, type);
   }
 
   saveCellValue = (value: string, key: string, rowId): void => {
-    if (value !== null && value !== undefined)
+    if (value !== null && value !== undefined) {
       switch (key) {
         case 'orgName':
           if (this.organisationsInvolved.some(x => x.id === rowId)) {
             this.organisationsInvolved.forEach(function (item) {
               if (item.id === rowId) {
-                item.name = value
+                item.name = value;
               }
             });
           }
@@ -134,12 +159,13 @@ export class CreateProgramPartnersComponent implements OnInit, OnUpdateCell {
           if (this.organisationsInvolved.some(x => x.id === rowId)) {
             this.organisationsInvolved.forEach(function (item) {
               if (item.id === rowId) {
-                item.contact = value
+                item.contact = value;
               }
             });
           }
           break;
       }
+    }
   }
 
 }
