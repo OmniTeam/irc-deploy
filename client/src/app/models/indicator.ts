9399e1211718a377a328b43c2d962e8b7d44a0ee
makwasi1 <crismakwasi12@gmail.com>
2022-06-24 14:50:17 +0300

    ch(add date on milestone in wrk plan)

--- client/src/app/models/indicator.ts
+++ client/src/app/models/indicator.ts
@@ -2,6 +2,8 @@ export class Indicator {
   id: string;
   name: string;
   milestoneId: string;
+  startDate: string;
+  endDate:string;
   overallTarget: string;
   disaggregation: {
     datePeriod: string,
