6bce3bd108405df20c3a461867cb72b18c2965bb
BrunoJay <brunojay001@gmail.com>
2022-06-29 17:17:45 +0300

    fixed submit report

--- client/src/app/mis-components/grant-process/grant-report.component.ts
+++ client/src/app/mis-components/grant-process/grant-report.component.ts
@@ -47,7 +47,8 @@ export class GrantReportComponent implements OnInit {
   }
 
   ngOnInit(): void {
-
+    console.log("isMakeCorrections",this.isMakeCorrections)
+    console.log("isReadOnly",this.isReadOnly)
     if (this.isReadOnly || this.isMakeCorrections) {
       let disabled = this.isReadOnly
       if(this.isMakeCorrections) disabled = false
