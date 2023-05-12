d4402919d1422f62fa9fb08662cda0b916f88887
makwasi1 <crismakwasi12@gmail.com>
2023-05-12 17:51:09 +0300

    clean up

--- client/src/app/models/user.ts
+++
@@ -1,7 +0,0 @@
-export class User {
-  username: string;
-  roles: any;
-  access_token: string;
-  refresh_token: string;
-  email: string;
-}
--- client/src/environments/environment.prod.ts
+++
@@ -1,4 +0,0 @@
-export const environment = {
-  production: true,
-  serverUrl: 'http://157.230.227.3:8082/irc'
-};
